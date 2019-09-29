d0a4801ada317bfb5c90f68a5eac61c3ef50510f
Fabian Lesniak <fabian@lesniak-it.de>
2019-09-29 16:33:08 +0200

    add macros.h

---
+++ macros.h
@@ -0,0 +1,18 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+#define LED_OFF(x,y,z) (currentImage[8*z+y] |= (1<<x))
+#define LED_ON(x,y,z) (currentImage[8*z+y] &= ~(1<<x))
+
+#define BACKLIGHT_ON (PORTB |= (1 << 0))
+#define BACKLIGHT_OFF (PORTB &= ~(1 << 0))
+
+#define RCK_ON (PORTB |= (1 << 2))
+#define RCK_OFF (PORTB &= ~(1 << 2))
+
+#define BUTTON_L (!(PIND & (1<<PD3)))
+#define BUTTON_R (!(PIND & (1<<PD2)))
+
+#define WAIT_UART_READY while(!(UCSRA & (1 << UDRE)))
+
+#endif //MACROS_H
